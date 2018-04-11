4736be98c4103d8379e79c62beef2e452c01847e
Carlos Alberto Macca <33333512+CarlosMacca@users.noreply.github.com>
2018-04-11 10:26:35 -0400

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,7 @@
 require("dotenv").config();
+require("dotenv").config();
+var fs = require("fs")
+var keys = require("./keys.js");
+var spotify = require('spotify');
+var spotifyKeys = new Spotify(keys.spotify);
+var clientKey = new Twitter(keys.twitter);
